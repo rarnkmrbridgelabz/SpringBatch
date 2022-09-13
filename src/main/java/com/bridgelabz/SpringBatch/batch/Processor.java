bdd5e7defcd246fba1fe4b8493b8a53a4dd7b279
Arun <rarnkmrbridgelabz.com>
2022-09-13 14:00:20 +0530

    Adding Sring Batch

---
+++ src/main/java/com/bridgelabz/SpringBatch/batch/Processor.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.SpringBatch.batch;
+
+import com.bridgelabz.SpringBatch.model.User;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class Processor implements ItemProcessor<User, User> {
+
+    private static final Map<String, String> DEPT_NAMES =
+            new HashMap<>();
+
+    public Processor() {
+        DEPT_NAMES.put("001", "Mechanical");
+        DEPT_NAMES.put("002", "Production");
+        DEPT_NAMES.put("003", "Automobile");
+    }
+
+    @Override
+    public User process(User user) throws Exception {
+        String deptCode = user.getDept();
+        String dept = DEPT_NAMES.get(deptCode);
+        user.setDept(dept);
+        user.setTime(new Date());
+        System.out.println(String.format("Converted from [%s] to [%s]", deptCode, dept));
+        return user;
+    }
+}
